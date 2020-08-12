662d940343e717aa6e6698d069a3e7e7ad3fa473
Analo-pez <ana.ts.lopez@gmail.com>
2020-08-12 17:49:07 +0200

    add projects information

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,27 @@ function About() {
     return (
         <section className="aboutPage">
             <Header />
-            <div className="aboutPage__box ">
-                <h1 className="aboutPage__box--title ">Sobre mí</h1>
+            <div className="aboutPage__box wrapper2">
                 <img className="aboutPage__box--img" src={imageAna} alt="Landing del proyecto" />
+                <section className="aboutPage__box--text">
+                    <p className="aboutPage__box--paragraph">Me defino como una aprendiz de la vida,
+			creciendo con cada experiencia transitada.</p>
+                    <p className="aboutPage__box--paragraph">He comenzado mi andadura en el mundo de la programación y mi aspiración es trabajar
+                    en proyectos que implementan grandes equipos
+                    humanos, donde compartir conocimientos y
+			diversidad.</p>
+                    <p className="aboutPage__box--paragraph">Defiendo que los resultados son mejores
+                    cuantas más perspectivas contemplan
+			porque "muchos pocos hacen un mucho".</p>
+                    <p className="aboutPage__box--paragraph">Como Desarrolladora Front-End puedo
+                    seguir creando, compartiendo y explotando
+                    mi creatividad al máximo.  ¡Quiero aportar lo mejor de mí y recalcular
+			mis límites!</p>
+                </section>
+                <Link to="/Home" className="buttonHome">
+                    <button>Home</button>
+                </Link >
             </div>
-            <Link to="/Home" className="buttonHome">
-                <button>Home</button>
-            </Link >
         </section>
     );
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,67 +1,85 @@
 import React from 'react';
 import Project from './Project';
 import ateam from "../images/ateam.png";
+import Botánical from "../images/Botánical.png";
+import RickMorty from "../images/RickMorty.png";
+import quoteGenerator from "../images/quoteGenerator.png";
+import futuramas from "../images/futuramas.png";
+import pokecards from "../images/pokecards.png";
+import seryes from "../images/seryes.png";
+import zapas from "../images/zapas.png";
+
 
 
 const ProjectList = props => {
 
     return (
-        <section className="main">
+        <section className="main wrapper">
             <Project
                 classArticle="project project1"
                 link="https://analo-pez.github.io/The-A-Team-project/"
                 titleLink="A-Team web"
                 srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                title="A-Team Web"
+                text="Web de presentación con diseño responsive y formulario de contacto de perfil profesional. Realizado con HTML5, CSS3, SASS"
             />
             <Project
                 classArticle="project project2"
-                link="https://analo-pez.github.io/The-A-Team-project/"
-                titleLink="A-Team web"
-                srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                link="http://analo-pez.github.io/Characters-Rick-y-Morty/"
+                titleLink="Characters Rick y Morty web"
+                srcImg={RickMorty}
+                title="Personajes Rick y Morty"
+                text="App web diseñada con React. Conectada a una API pública de personajes de la serie Rick y Morty, permite buscar y conocer a cada uno de ellos"
             />
             <Project
-                classArticle="project"
-                link="https://analo-pez.github.io/The-A-Team-project/"
-                titleLink="A-Team web"
-                srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                classArticle="project project3"
+                link="https://analo-pez.github.io/Botanical-2.0-App/"
+                titleLink="Botánical web"
+                srcImg={Botánical}
+                title="Botánical 2.0 Profile-Card"
+                text="Proyecto de código heredado realizado en equipo utilizando REACT"
             />
             <Project
-                classArticle="project"
-                link="https://analo-pez.github.io/The-A-Team-project/"
-                titleLink="A-Team web"
-                srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                classArticle="project project4"
+                link="https://analo-pez.github.io/QuotesGenerator/"
+                titleLink="QuotesGenerator web"
+                srcImg={quoteGenerator}
+                title="Quotes Generator"
+                text="Generador de frases del día, con contenido propio. Desarrollado con JavaScript, HTML5, CSS3, SASS"
             />
             <Project
-                classArticle="project"
-                link="https://analo-pez.github.io/The-A-Team-project/"
-                titleLink="A-Team web"
-                srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                classArticle="project project5"
+                link="https://analo-pez.github.io/FuturAmas-Card-Profile/"
+                titleLink="Futuramas web"
+                srcImg={futuramas}
+                title="FuturAmas del Code"
+                text="Web para diseño de profile cards personalizadas. Diseñado con JavaScript, HTML5, CSS3, SASS"
             />
             <Project
-                classArticle="project"
-                link="https://analo-pez.github.io/The-A-Team-project/"
-                titleLink="A-Team web"
-                srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                classArticle="project project6"
+                link="http://analo-pez.github.io/PokeCards/"
+                titleLink="Pokecards web"
+                srcImg={pokecards}
+                title="PokeCards"
+                text="Web app realizada utilando API´s públicas y desarrollada
+                con REACT"
             />
             <Project
-                classArticle="project"
+                classArticle="project project7"
+                link="http://analo-pez.github.io/SerYes-finder/"
+                titleLink="Ser-yes web"
+                srcImg={seryes}
+                title="Ser¡yes!"
+                text="Buscador de series realizado utilizando API´s públicas y
+                desarrollado con JavaScript, HTML5, CSS3, SASS"
+            />
+            <Project
+                classArticle="project project8"
                 link="https://analo-pez.github.io/The-A-Team-project/"
                 titleLink="A-Team web"
-                srcImg={ateam}
-                title="Web grupal"
-                text="Web de presentación para dar a conocer el perfil profesional. Realizado con HTML5, CSS3, SASS."
+                srcImg={zapas}
+                title="Portfolio"
+                text="Portfolio profesional realizado con React, en el que se implementa el uso de React Router"
             />
         </section>
     );
