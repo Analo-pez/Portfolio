c40685721a34a027efeb1d4c3b4795ba15940864
Analo-pez <ana.ts.lopez@gmail.com>
2020-08-17 14:25:58 +0200

    create code link

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,6 +7,7 @@ function Project(props) {
             <a className="project__link" href={props.link} title={props.titleLink} target="_blank" rel="noopener noreferrer">
                 <img className="project__img" src={props.srcImg} alt={props.titleLink} />
             </a>
+            <a className="project__codeLink" href={props.codeUrl} target="_blank" rel="noopener noreferrer">Code</a>
             <div className="project__box">
                 <h3 className="project__box--title">{props.title}</h3>
                 <p className="project__box--text">{props.text} </p>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -20,6 +20,7 @@ const ProjectList = props => {
                 link="https://analo-pez.github.io/The-A-Team-project/"
                 titleLink="A-Team web"
                 srcImg={ateam}
+                codeUrl="https://github.com/Analo-pez/The-A-Team-project"
                 title="A-Team Web"
                 text="Web de presentación con diseño responsive y formulario de contacto de perfil profesional. Realizado con HTML5, CSS3, SASS"
             />
@@ -28,6 +29,7 @@ const ProjectList = props => {
                 link="http://analo-pez.github.io/Characters-Rick-y-Morty/"
                 titleLink="Characters Rick y Morty web"
                 srcImg={RickMorty}
+                codeUrl="https://github.com/Analo-pez/Characters-Rick-y-Morty"
                 title="Personajes Rick y Morty"
                 text="App web diseñada con React. Conectada a una API pública de personajes de la serie Rick y Morty, permite buscar y conocer a cada uno de ellos"
             />
@@ -36,6 +38,7 @@ const ProjectList = props => {
                 link="https://analo-pez.github.io/Botanical-2.0-App/"
                 titleLink="Botánical web"
                 srcImg={Botánical}
+                codeUrl="https://github.com/Analo-pez/Botanical-2.0-App"
                 title="Botánical 2.0 Profile-Card"
                 text="Proyecto de código heredado realizado en equipo utilizando REACT"
             />
@@ -44,6 +47,7 @@ const ProjectList = props => {
                 link="https://analo-pez.github.io/QuotesGenerator/"
                 titleLink="QuotesGenerator web"
                 srcImg={quoteGenerator}
+                codeUrl="https://github.com/Analo-pez/QuotesGenerator"
                 title="Quotes Generator"
                 text="Generador de frases del día, con contenido propio. Desarrollado con JavaScript, HTML5, CSS3, SASS"
             />
@@ -52,6 +56,7 @@ const ProjectList = props => {
                 link="https://analo-pez.github.io/FuturAmas-Card-Profile/"
                 titleLink="Futuramas web"
                 srcImg={futuramas}
+                codeUrl="https://github.com/Analo-pez/FuturAmas-Card-Profile"
                 title="FuturAmas del Code"
                 text="Web para diseño de profile cards personalizadas. Diseñado con JavaScript, HTML5, CSS3, SASS"
             />
@@ -60,6 +65,7 @@ const ProjectList = props => {
                 link="http://analo-pez.github.io/PokeCards/"
                 titleLink="Pokecards web"
                 srcImg={pokecards}
+                codeUrl="https://github.com/Analo-pez/PokeCards"
                 title="PokeCards"
                 text="Web app realizada utilando API´s públicas y desarrollada
                 con REACT"
@@ -69,6 +75,7 @@ const ProjectList = props => {
                 link="http://analo-pez.github.io/SerYes-finder/"
                 titleLink="Ser-yes web"
                 srcImg={seryes}
+                codeUrl="https://github.com/Analo-pez/SerYes-finder"
                 title="Ser¡yes!"
                 text="Buscador de series realizado utilizando API´s públicas y
                 desarrollado con JavaScript, HTML5, CSS3, SASS"
@@ -78,6 +85,7 @@ const ProjectList = props => {
                 link="./"
                 titleLink="Portfolio web"
                 srcImg={portfolio}
+                codeUrl="https://github.com/Analo-pez/Portfolio"
                 title="Portfolio"
                 text="¡Este mismo! Portfolio profesional realizado con React, en el que se implementa el uso de React Router"
             />
